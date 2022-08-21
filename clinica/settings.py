9b56ed51d527108e0638efd4e1f9aff640061942
kelver <kelverwt@gmail.com>
2022-08-21 16:37:50 -0300

    commit local

--- clinica/settings.py
+++ clinica/settings.py
@@ -18,7 +18,6 @@ from dj_database_url import parse as dburl
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
